4aac676fd330c5d7d7fe4a1f04bd1c1fd8fcad3f
Restyled.io <commits@restyled.io>
2022-12-17 14:00:50 +0000

    Restyled by prettier

--- src/bot.js
+++ src/bot.js
@@ -1,24 +1,25 @@
 // load .env file
-require('dotenv').config()
+require("dotenv").config();
 
 // Require the necessary discord.js classes
-const {Client, Events, GatewayIntentBits} = require('discord.js');
+const { Client, Events, GatewayIntentBits } = require("discord.js");
 
 // Create a new client instance
 const client = new Client({
-  intents : [
+  intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMembers,
-  ]
+  ],
 });
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already
 // defined 'client'
-client.once(Events.ClientReady,
-            c => { console.log(`Ready! Logged in as ${c.user.tag}`); });
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
 
 // Log in to Discord with your client's token
 client.login(process.env.BOT_TOKEN);
